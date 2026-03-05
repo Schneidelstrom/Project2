56cfbc81586ab2563e42db563a3e8921baed226e
Schneidelstrom <jipetilla@up.edu.ph>
2026-03-05 21:34:22 +0800

    Added audio

--- src/main/java/cmsc125/lab3/controllers/AppController.java
+++ src/main/java/cmsc125/lab3/controllers/AppController.java
@@ -2,13 +2,16 @@ package cmsc125.lab3.controllers;
 
 import cmsc125.lab3.views.DashboardView;
 import cmsc125.lab3.views.MainFrame;
+import cmsc125.lab3.services.AudioService;
 import javax.swing.Timer;
 
 public class AppController {
     private final MainFrame mainFrame;
+    private final AudioService audioService;
 
     public AppController(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
+        this.audioService = new AudioService();
         setupButtonListeners();
     }
 
@@ -34,7 +37,12 @@ public class AppController {
 
     // Splash screen 3 seconds delay for loading animation
     public void startApplication() {
-        Timer transitionTimer = new Timer(1000, e -> mainFrame.showDashboard());
+        audioService.playMusic("/audio/opening.wav");
+        Timer transitionTimer = new Timer(3000, e -> {
+            audioService.stopMusic();   // Stop music exactly when timer finishes
+            mainFrame.showDashboard();  // Switch View to Dashboard
+        });
+
         transitionTimer.setRepeats(false); // Run once
         transitionTimer.start();
     }
--- src/main/java/cmsc125/lab3/services/AudioService.java
+++ src/main/java/cmsc125/lab3/services/AudioService.java
@@ -1,4 +1,41 @@
 package cmsc125.lab3.services;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
 public class AudioService {
+    private Clip currentClip; // Holds audio in memory to play/stop it
+
+    // Method to load and play .wav file
+    public void playMusic(String filePath) {
+        try {
+            // Look for audio file in resources folder
+            URL audioURL = getClass().getResource(filePath);
+            if (audioURL == null) {
+                System.err.println("Warning: Could not find audio file at " + filePath);
+                return; // Exit to avoid crash
+            }
+
+            // Load audio and open clip
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioURL);
+            currentClip = AudioSystem.getClip();
+            currentClip.open(audioStream);
+
+            // Start playing
+            currentClip.start();
+
+        } catch (Exception e) {
+            System.err.println("Error playing audio: " + e.getMessage());
+        }
+    }
+
+    // Method to safely stop music and free up computer memory
+    public void stopMusic() {
+        if (currentClip != null && currentClip.isRunning()) {
+            currentClip.stop();
+            currentClip.close();
+        }
+    }
 }
